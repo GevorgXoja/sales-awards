561375f1b0e9520a75548b1e39b437ab51267566
Gevor <gev.xodgayn@gmail.com>
2025-10-25 09:37:15 +0400

    Initial commit of Spring Boot project structure from start.spring.io

---
+++ src/main/java/com/lpharma/SalesAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.lpharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesAwardsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SalesAwardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpharma/SalesAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
